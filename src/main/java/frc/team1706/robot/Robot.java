307d00a79a28edc83ec38f888b61e5b54a82fd17
Joey Flynn <JoeF140@yahoo.com>
2017-11-08 21:57:33 -0600

    Git test

--- src/main/java/frc/team1706/robot/Robot.java
+++ src/main/java/frc/team1706/robot/Robot.java
@@ -35,7 +35,7 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-//Git test 6
+//Git test 7
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as described in the IterativeRobot
